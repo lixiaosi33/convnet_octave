3b4f5581c345625448afe081ca9cda40a0a2a68c
Shashwat Kumar <thegreatshasha@gmail.com>
2015-09-16 23:04:42 +0530

    add nesterov's momentum method

--- cnn/minFuncSGD.m
+++ cnn/minFuncSGD.m
@@ -36,7 +36,7 @@ m = length(labels); % training set size
 mom = 0.5;
 momIncrease = 20;
 velocity = zeros(size(theta));
-
+look_ahead=zeros(size(theta));
 %%======================================================================
 %% SGD loop
 it = 0;
@@ -60,15 +60,16 @@ for e = 1:epochs
 
         % evaluate the objective function on the next minibatch
         [cost grad] = funObj(theta,mb_data,mb_labels);
-        
         % Instructions: Add in the weighted velocity vector to the
         % gradient evaluated above scaled by the learning rate.
         % Then update the current weights theta according to the
         % sgd update rule
         
         %%% YOUR CODE HERE %%%
-        velocity = mom*velocity + alpha * grad;
-        theta = theta - velocity;
+        look_ahead = theta+mom*velocity;
+        [cost2 grad_ahead] = funObj(look_ahead,mb_data,mb_labels);
+        velocity = mom*velocity - alpha*grad_ahead;
+        theta =theta+velocity;
         
         fprintf('Epoch %d: Cost on iteration %d is %f\n',e,it,cost);
 
